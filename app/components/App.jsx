337d69b507a52414a8f076ebeb5ea30d71e024eb
Tomasz Jósiak <t.josiak@merixstudio.com>
2017-07-28 07:28:30 +0200

    Adding lead pragraph

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -30,7 +30,8 @@ function appAnswer(quizStore) {
   const all = quizStore.questions.length;
   return (
     <div className="app__container app__container--relative">
-      <p className="app__lead">Welcome, {quizStore.applicant}</p>
+      <p className="app__heading">Welcome, {quizStore.applicant}</p>
+      <p className="app__lead">Aenean eleifend tempor lacus, in cursus nunc hendrerit sit amet. Fusce sollicitudin augue sed maximus efficitur. Nunc sed sodales dolor, sit amet interdum lacus. Sed scelerisque dolor pharetra dolor aliquet vulputate. Donec blandit at neque quis condimentum. Aliquam erat volutpat. Nam sit amet condimentum arcu, rhoncus ultricies orci. Suspendisse sapien purus, sagittis in metus eu, semper varius nunc. In hac habitasse platea dictumst. Aliquam auctor massa a orci hendrerit, vitae hendrerit ipsum viverra.</p>
       <div className={`app__done ${saved === all ? 'app__done--all' : ''}`}>
         {saved}/{all}
       </div>
