7a808a146079e41f1dbeee67be3a645fe0007ca8
Tomasz Jósiak <t.josiak@merixstudio.com>
2017-08-16 11:32:44 +0200

    Changing lead content.

--- app/components/Lead.jsx
+++ app/components/Lead.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
+/* eslint-disable max-len */
 
 const Lead = ({ applicant }) => (
   <div className="lead">
     <h2 className="lead__heading">
-      Welcome, {applicant}
+      Hello, {applicant}
     </h2>
     <p className="lead__paragraph">
-      Aenean eleifend tempor lacus, in cursus nunc hendrerit sit amet.
-      Fusce sollicitudin augue sed maximus efficitur.
-      Nunc sed sodales dolor, sit amet interdum lacus
-      Sed scelerisque dolor pharetra dolor aliquet vulputate.
-      Donec blandit at neque quis condimentum.
+      Time has come for us to meet. You probably already know that work culture at our company is a huge deal for us. But that’s not all - you should be not only a team player but also one of the most valuable players who will bring to Merixstudio the experience and knowledge. This is why we have the following test for you. Please, fill it in, and we will see if we’re the right match. Good luck!
     </p>
   </div>
 );
+/* eslint-nable max-len */
 
 Lead.propTypes = {
   applicant: PropTypes.string.isRequired,
